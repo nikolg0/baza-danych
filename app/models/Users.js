7350b685f1605f39b3a6e68477644c00d2576e0d
nikolg0 <nikig0731@gmail.com>
2024-06-20 20:19:44 +0200

    pierwszy commit

---
+++ app/models/Users.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  name: String,
+  address: {
+    street: String,
+    suite: String,
+    city: String,
+    zipcode: String,
+    website: String,
+  },
+});
+
+module.exports = mongoose.model("User", User);
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const hbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/baza-danych");
+
+const User = require("./app/models/Users");
+
+app.use("/files", express.static("public"));
+
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+/* wyświetlanie użytkownika przez id */
+app.get("/mongoose", function (req, res) {
+  User.findById("6672d3ded7a53c9ab71134c8")
+    .then((user) => {
+      res.render("home", {
+        name: user.name,
+        address: user.address,
+        website: user.website,
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+/* wyświetlanie wszystkich użytkowników */
+app.get("/mongoose", function (req, res) {
+  Users.find()
+    .then((users) => {
+      res.render("home", { users });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.listen(8080, function () {
+  console.log("Serwer Node.js działa");
+});
