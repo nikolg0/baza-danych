8569514532b8899787cf88bddd54f2c2ac0701a8
nikolg0 <nikig0731@gmail.com>
2024-06-22 15:15:28 +0200

    last commit

--- index.js
+++ index.js
@@ -7,30 +7,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/baza-danych");
 
 const User = require("./app/models/Users");
 
-app.use("/files", express.static("public"));
-
 app.engine("hbs", hbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
-/* wyświetlanie użytkownika przez id */
-app.get("/mongoose", function (req, res) {
-  User.findById("6672d3ded7a53c9ab71134c8")
-    .then((user) => {
-      res.render("home", {
-        name: user.name,
-        address: user.address,
-        website: user.website,
-      });
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
 /* wyświetlanie wszystkich użytkowników */
-app.get("/mongoose", function (req, res) {
+app.get("/", function (req, res) {
   User.find()
+    .lean()
     .then((users) => {
+      console.log(users);
       res.render("home", { users });
     })
     .catch((err) => {
